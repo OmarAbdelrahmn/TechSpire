152e21dba1d7c7597cd4bb36fe1fca194f7db6ff
omar abdo <omarfaroq2003@gmail.com>
2025-05-13 13:12:55 +0300

    add get stages

---
+++ TechSpire.Application/Contracts/Stage/StageResponse.cs
@@ -0,0 +1,12 @@
+﻿
+using TechSpire.Domain.Entities;
+
+namespace TechSpire.Application.Contracts.Stage;
+public record StageResponse(
+    int Id,
+    string Name,
+    List<Lesson> Lessons,
+    List<Post> Posts,
+    List<Article> Articles,
+    List<Book> Books
+    );
---
+++ TechSpire.Application/Services/IStageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechSpire.Application.Abstraction;
+using TechSpire.Application.Contracts.Stage;
+
+namespace TechSpire.Application.Services;
+public interface IStageService
+{
+    Task<Result<IEnumerable<StageResponse>>>
+}
